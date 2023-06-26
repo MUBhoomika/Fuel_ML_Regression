2ad29f29f64df40eb24736b52df16cf3038526c3
MUBhoomika <137698669+MUBhoomika@users.noreply.github.com>
2023-06-26 16:19:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def main():
     # front end elements of the web page
     html_temp = """
     <div style ="background-color:Blue;padding:13px">
-    <h1 style ="color:black;text-align:center;"> Bhoomika@ Pragyan AI and FUEL Walmart Sale Prediction ML App</h1>
+    <h1 style ="color:white;text-align:center;"> Bhoomika@ Pragyan AI and FUEL Walmart Sale Prediction ML App</h1>
     </div>
     """
 
